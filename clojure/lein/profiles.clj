c74da624cdd1a51c5f131929da6c1c9c28b16855
Zlatozar Zhelyazkov <z_zhelyazkov@prosyst.bg>
2018-03-22 11:43:34 +0200

    Remove clojure add F#

--- clojure/lein/profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user
- ;; https://github.com/technomancy/leiningen/wiki/Plugins
- {:plugins [[lein-ancient "0.6.7"]
-            [cider/cider-nrepl "0.9.0"]]
-
-  :dependencies [[clojure-complete "0.2.3"]
-                 [org.clojure/tools.nrepl "0.2.10"]
-                 [org.clojure/tools.trace "0.7.8"]
-                 [instaparse "1.4.0"]]}}
